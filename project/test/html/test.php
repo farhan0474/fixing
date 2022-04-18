0a5a949737f954a78e75d37c98f5937ccef71e69
ibrahim <mahmoud2@uwindsor.ca>
2022-04-18 02:01:49 -0400

    title update

--- project/test/html/test.php
+++ project/test/html/test.php
@@ -8,7 +8,7 @@ session_start();
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Miranda</title>
+      <title>Miranda - Shoe Store</title>
       <link rel="stylesheet" href="../css/main.css">
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> 
       
